c4ab826dc7db906808b6dcfd64df09ade49ff490
Connor Lundi <cman2399@aol.com>
2015-10-29 10:52:47 -0400

    Added classes

--- Pigo-lundic.py
+++ Pigo-lundic.py
@@ -1,9 +1,27 @@
-#from gopigo import *
-
-__author__ = 'Connor'
+from gopigo import *
+import time
 
 class Pigo:
+
+    isMoving = False
+    servoPos = 90
+
     def __init__(self):
-        print "I'm such a pigp. beep beep."
+        print "I'm a little robot car. beep beep."
+
+    def stop(self):
+        self.isMoving = False
+        while stop()!= 1:
+            time.sleep(.1)
+            print "Ummm, yeah I can't stop!"
+
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.sleep(.1)
+            print "Hey, I can't seem to get moving"
 
 tina = Pigo()
+tina.fwd()
+time.sleep(2)
+tina.stop()
