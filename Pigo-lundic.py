3645fe52efed054c9d4de00a25cd8544b012eb06
Connor Lundi <cman2399@aol.com>
2015-11-12 11:00:26 -0500

    Added Dance command

--- Pigo-lundic.py
+++ Pigo-lundic.py
@@ -57,20 +57,40 @@ class Pigo:
     def circleLeft(self):
         for x in range(1):
             left()
-        time.sleep(3)
+        time.sleep(0.6)
         self.stop()
 
     def circleRight(self):
         for x in range(1):
             right()
-        time.sleep(3)
+        time.sleep(0.6)
         self.stop()
 
+    def shuffle(self):
+        for x in range(2):
+            left_rot()
+            right_rot()
+        time.sleep(0.5)
+        self.stop()
+
+    def servoShake(self):
+        for x in range(40,60):
+            servo(x)
+            time.sleep(.1)]
+        #time.sleep(0.1)
+        self.stop
+
+
+    def blink(self):
+        for x in range(4):
+            led_on(1)
+            time.sleep(0.3)
+            led_off(1)
+        self.stop()
 
     #######
     #######  ADVANCED METHODS
     #######
-
     def dance(self):
         print "I just want to DANCE!"
         if self.keepGoing():
