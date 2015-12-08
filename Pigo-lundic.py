0dd0e5fab0d8f2d1ef53da1ed98e2b98258dc256
Connor Lundi <cman2399@aol.com>
2015-12-08 11:58:19 -0500

    fixed findAngle

--- Pigo-lundic.py
+++ Pigo-lundic.py
@@ -122,6 +122,7 @@ class Pigo:
             if counter >= (20/self.STEPPER):
                 print "We've found an option at angle " + str(ang - 10)
                 counter = 0
+                return ang - 10
                 option[0] = (ang - 10)
                 optindex += 1
         if self.status['wentleft']:
