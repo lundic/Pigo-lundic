62606be3e22e57383bfc2a8c1aace9844e5ac484
Connor Lundi <cman2399@aol.com>
2015-12-08 11:54:30 -0500

    fixed findAngle

--- Pigo-lundic.py
+++ Pigo-lundic.py
@@ -112,7 +112,7 @@ class Pigo:
 
     def findAngle(self):
         counter = 0
-        option = []  #we're going to fill this array with the angles of open paths
+        option = [] * 10  #we're going to fill this array with the angles of open paths
         optindex = 0  #this starts at 0 and will increase every time we find an option
         for ang in range(20, 160, self.STEPPER):
             if sweep[ang] > self.MIN_DIST:
